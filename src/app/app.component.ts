871c0385af06571787bbdd47256cb8eae5cf3b1b
redluigi64 <aldo.averza@gmail.com>
2018-01-06 20:30:15 -0500

    Test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,33 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  errorText = '';
+  productName ='';
+  productDesc = '';
+  productSku = '';
+  products: string[][] = [];
+
+
+  addProduct(){
+    if (this.productName == '' || this.productDesc == '' || this.productSku == '') //valida que todos los datos estén llenos
+    {
+      this.errorText = 'Por favor llenar todos los campos';
+      setTimeout (() => {
+        this.errorText = '';
+      }, 2000); //muestra el mensaje por dos segundos
+    }
+    else
+    {
+      this.products.push([this.productName, this.productDesc, this.productSku]);
+      this.productName ='';
+      this.productDesc = '';
+      this.productSku = '';
+    }//introduce productos al array y limpia los campos
+  }
+
+  deleteRow(index) {
+      this.products.splice(index, 1);//borra la fila seleccionada
+    }
+
+
 }
